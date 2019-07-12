afba1779897cbc05e4127d8a28f9745b8db68383
Sampath Satti <sampathsatti@gmail.com>
2019-07-12 13:02:18 -0700

    Scatter and cdf plot reversed

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import math
 import csv
 import brewer2mpl
 
-e = xml.etree.ElementTree.parse('Neutrophils_fMLP_Tracks_5.xml').getroot()
+e = xml.etree.ElementTree.parse('Neutrophils_fMLP_Tracks_12.xml').getroot()
 
 bmap = brewer2mpl.get_map('Set2', 'qualitative', 8)
 colors = bmap.mpl_colors
@@ -80,10 +80,6 @@ for track in e.iter('particle'):
 
 
 
-    initialPosX.append(x[0])
-    finalPosX.append(x.pop())
-    initialPosY.append(y[0])
-    finalPosY.append(y.pop())
 
 print(np.random.randint(10, size=(1, 2)))
 plt.axis('square')
@@ -118,7 +114,7 @@ plt.gca().spines['right'].set_visible(False)
 plt.gca().spines['top'].set_visible(False)
 plt.gca().spines['left'].set_position('zero')
 plt.gca().spines['bottom'].set_position('zero')
-plt.axis([-350, 350, -250, 800])
+plt.axis([-350, 350, -250, 1000])
 plt.xticks([-300, -150, 0, 150, 300])
 plt.gca().set_aspect('equal')
 plt.show()
@@ -127,6 +123,15 @@ print(len(initialPosX))
 print(len(finalPosX))
 
 plt.figure(3)
+
+#flip data around to ensure continuity with figure 5
+
+
+for i in range(0, len(finalPosX)-1, 1):
+    finalPosX[i] = 2000-finalPosX[i]
+
+for i in range(0, len(initialPosX)-1, 1):
+    initialPosX[i] = 2000-initialPosX[i]
 
 for i in range(0, len(finalPosX)-1, 1):
     after.append(finalPosX[i])
@@ -137,22 +142,22 @@ for i in range(0, len(finalPosX)-1, 1):
 
 
 num_bins = 40
-# the histogram of the data
-n, bins, patches = plt.hist(after, num_bins, histtype=u'step',  density=1, facecolor='tab:blue', alpha=0.5)
-n, bins, patches = plt.hist(before, num_bins,  histtype=u'step',  density=1, facecolor='tab:orange', alpha=0.5)
+
+
+n, bins, patches = plt.hist(after, num_bins, histtype=u'step', cumulative=True, density=1, facecolor='tab:blue', alpha=0.7)
+n, bins, patches = plt.hist(before, num_bins,  histtype=u'step', cumulative=True, density=1, facecolor='tab:orange', alpha=0.7)
 plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
 plt.xlabel('Position along axis', size=12)
 plt.ylabel('Cumulative distribution function', size=12)
-plt.title(r'Cell Distribution w/o alignment', size=16)
+
 
 plt.show()
 
+plt.figure(4)
 
-n, bins, patches = plt.hist(after, num_bins, histtype=u'step', cumulative=True, density=1, facecolor='tab:blue', alpha=0.5)
-n, bins, patches = plt.hist(before, num_bins,  histtype=u'step', cumulative=True, density=1, facecolor='tab:orange', alpha=0.5)
-plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
-plt.xlabel('Position along axis', size=12)
-plt.ylabel('Cumulative distribution function', size=12)
-plt.title(r'Cell Distribution w/o alignment', size=16)
+for i in range(1, len(finalPosY), 1):
+    plt.scatter(finalPosX[i], finalPosY[i], c='tab:blue', s=50, alpha=0.7, edgecolors='none')
+for i in range(1, len(initialPosX), 1):
+    plt.scatter(initialPosX[i], initialPosY[i], c='tab:orange', s=50, alpha=0.7, edgecolors='none')
 
 plt.show()
